95b44d481f6d65db393c25382467e913433c90f8
Futurestar7891 <gajanandmali69@gmail.com>
2024-08-15 22:21:42 +0530

    hello

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,4 +1,4 @@
-import { configureStore,combineReducers} from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage
 import productsReducer from "../Redux/ProductSlice";
@@ -8,33 +8,49 @@ import visitedReducer from "../Redux/Recentslice";
 import forgotpassReducer from "../Redux/ForgotSlice";
 import verifyotpReducer from "../Redux/VerifyotpSlice";
 import changepasswordReducer from "../Redux/ChangepassSlice";
-import CartdataReducer from '../Redux/CartSlice';
-import ReviewReducer from '../Redux/ReviewSlice';
-import BuyproductReducer from '../Redux/BuyproductSlice'
-import  AddressReducer  from "../Redux/Addresses";
-import ShippingaddressReducer from '../Redux/Shippingaddress'
+import CartdataReducer from "../Redux/CartSlice";
+import ReviewReducer from "../Redux/ReviewSlice";
+import BuyproductReducer from "../Redux/BuyproductSlice";
+import AddressReducer from "../Redux/Addresses";
+import ShippingaddressReducer from "../Redux/Shippingaddress";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
   // Optionally, you can whitelist specific reducers to be persisted
-  whitelist: ['product', 'category', 'filter', 'visited', 'forgotpass', 'verifyotp', 'changepass', 'Cart', 'Review','Buyproduct','Address','Shippingaddress'],
+  whitelist: [
+    "product",
+    "category",
+    "filter",
+    "visited",
+    "forgotpass",
+    "verifyotp",
+    "changepass",
+    "Cart",
+    "Review",
+    "Buyproduct",
+    "Address",
+    "Shippingaddress",
+  ],
 };
 
-const persistedReducer = persistReducer(persistConfig, combineReducers({
-  product: productsReducer,
-  category: categoryReducer,
-  filter: filterReducer,
-  visited: visitedReducer,
-  forgotpass: forgotpassReducer,
-  verifyotp: verifyotpReducer,
-  changepass: changepasswordReducer,
-  Cart: CartdataReducer,
-  Review: ReviewReducer,
-  Buyproduct:BuyproductReducer,
-  Address:AddressReducer,
-  Shippingaddress:ShippingaddressReducer
-}));
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    product: productsReducer,
+    category: categoryReducer,
+    filter: filterReducer,
+    visited: visitedReducer,
+    forgotpass: forgotpassReducer,
+    verifyotp: verifyotpReducer,
+    changepass: changepasswordReducer,
+    Cart: CartdataReducer,
+    Review: ReviewReducer,
+    Buyproduct: BuyproductReducer,
+    Address: AddressReducer,
+    Shippingaddress: ShippingaddressReducer,
+  })
+);
 
 export const store = configureStore({
   reducer: persistedReducer,
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 // import { store } from './Redux/Store.js'
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import { store, persistor } from './Redux/Store.js';
+// import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./Redux/Store.js";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
-   
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
